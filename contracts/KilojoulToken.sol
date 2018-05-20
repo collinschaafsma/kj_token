02fba2f4dcdc28963c673dc1d4ae2fc76692b24e
Collin Schaafsma <collin@collin.io>
2018-05-20 09:13:49 -0600

    constant vars

--- contracts/KilojoulToken.sol
+++ contracts/KilojoulToken.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.4.23;
 import "openzeppelin-zos/contracts/token/ERC20/MintableToken.sol";
 
 contract KilojoulToken is MintableToken {
-  string public symbol = "KJT";
-  string public name = "Kilojoul Token";
-  uint8 public decimals = 18;
+  string public constant symbol = "KJT";
+  string public constant name = "Kilojoul Token";
+  uint8 public constant decimals = 18;
   uint256 public x;
 
   function initialize(uint256 _x) isInitializer("KilojoulToken", "0") public {
