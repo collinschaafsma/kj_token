9e9985ba833a1705e6d384cdd5fd6776c7de6e9c
Collin Schaafsma <collin@collin.io>
2018-05-20 09:41:38 -0600

    adding ropsten_infura network

--- truffle.js
+++ truffle.js
@@ -1,9 +1,17 @@
+require('dotenv').config();
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*"
+    },
+    ropsten_infura: {
+      provider: new HDWalletProvider(process.env.MNENOMIC, "https://ropsten.infura.io/" + process.env.INFURA_API_KEY),
+      network_id: 3,
+      gas: 4500000
     }
   }
 };
