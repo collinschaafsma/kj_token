454ec625f16bc87a9aca6426e04cb83280f0231a
Collin Schaafsma <collin@collin.io>
2018-05-20 09:04:39 -0600

    remove total supply because it's mintable, move name sym out of contructor

--- contracts/KilojoulToken.sol
+++ contracts/KilojoulToken.sol
@@ -3,17 +3,12 @@ pragma solidity ^0.4.23;
 import "openzeppelin-zos/contracts/token/ERC20/MintableToken.sol";
 
 contract KilojoulToken is MintableToken {
-  string public symbol;
-  string public  name;
-  uint8 public decimals;
-  uint public _totalSupply;
+  string public symbol = "KJT";
+  string public name = "Kilojoul Token";
+  uint8 public decimals = 18;
   uint256 public x;
 
   function initialize(uint256 _x) isInitializer("KilojoulToken", "0") public {
     x = _x;
-    symbol = "KJT";
-    name = "Kilojoul Token";
-    decimals = 2;
-    _totalSupply = 100000000000000000000000000;
   }
 }
